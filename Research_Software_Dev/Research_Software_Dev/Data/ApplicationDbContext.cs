57e384305ab88bbad2b83c9bc5107dfae43f472f
Bergen Cunningham <BergCu2001@gmail.com>
2024-10-19 23:15:15 -0600

    Initial Setup
    New project on Visual Studio

---
+++ Research_Software_Dev/Research_Software_Dev/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Research_Software_Dev.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
