cac322b31ff82958fdd3285e3487c0b187d6fa44
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-11-23 00:14:59 -0700

    added data annotations to Session model

--- Research_Software_Dev/Research_Software_Dev/Models/Sessions/Session.cs
+++ Research_Software_Dev/Research_Software_Dev/Models/Sessions/Session.cs
@@ -1,16 +1,32 @@
 ﻿using Research_Software_Dev.Models.Studies;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Research_Software_Dev.Models.Sessions
 {
     public class Session
     {
+        [Key]
         public int SessionId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
         public DateOnly Date { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
         public TimeOnly TimeStart { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
         public TimeOnly TimeEnd { get; set; }
 
         //Study Fk
+        [Required]
+        [ForeignKey("Study")]
         public int StudyId { get; set; }
+
+        [Required]
         public Study Study { get; set; }
 
 
