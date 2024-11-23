90ad64b7566752f17ed023289e678cea29d193f7
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-11-23 11:57:24 -0700

    added constructor to model class Role

--- Research_Software_Dev/Research_Software_Dev/Models/Roles/Role.cs
+++ Research_Software_Dev/Research_Software_Dev/Models/Roles/Role.cs
@@ -2,13 +2,13 @@
 
 namespace Research_Software_Dev.Models.Roles
 {
-    public class Role
+    public class Role(int id, string roleName)
     {
         [Key]
-        public int RoleId { get; set; }
+        public int RoleId { get; set; } = id;
 
         [Required]
         [StringLength(255)]
-        public string RoleName { get; set; }
+        public string RoleName { get; set; } = roleName;
     }
 }
