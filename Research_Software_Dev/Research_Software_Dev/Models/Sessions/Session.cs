00f1cabf894710b9559de19c462104c096bac05f
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-11-23 11:57:14 -0700

    added constructor to model class Session

--- Research_Software_Dev/Research_Software_Dev/Models/Sessions/Session.cs
+++ Research_Software_Dev/Research_Software_Dev/Models/Sessions/Session.cs
@@ -4,27 +4,27 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Research_Software_Dev.Models.Sessions
 {
-    public class Session
+    public class Session(int id, DateOnly date, TimeOnly timeStart, TimeOnly timeEnd, int studyId)
     {
         [Key]
-        public int SessionId { get; set; }
+        public int SessionId { get; set; } = id;
 
         [Required]
         [DataType(DataType.Date)]
-        public DateOnly Date { get; set; }
+        public DateOnly Date { get; set; } = date;
 
         [Required]
         [DataType(DataType.Time)]
-        public TimeOnly TimeStart { get; set; }
+        public TimeOnly TimeStart { get; set; } = timeStart;
 
         [Required]
         [DataType(DataType.Time)]
-        public TimeOnly TimeEnd { get; set; }
+        public TimeOnly TimeEnd { get; set; } = timeEnd;
 
         //Study Fk
         [Required]
         [ForeignKey("Study")]
-        public int StudyId { get; set; }
+        public int StudyId { get; set; } = studyId;
 
         [Required]
         public Study Study { get; set; }
