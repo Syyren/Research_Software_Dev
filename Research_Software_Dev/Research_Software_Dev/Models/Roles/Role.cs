07fb8bafd2e77a8316cda8fba6f731cc9e71d26d
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-11-23 00:14:42 -0700

    Added data annotations to Role model

--- Research_Software_Dev/Research_Software_Dev/Models/Roles/Role.cs
+++ Research_Software_Dev/Research_Software_Dev/Models/Roles/Role.cs
@@ -1,8 +1,14 @@
-﻿namespace Research_Software_Dev.Models.Roles
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Research_Software_Dev.Models.Roles
 {
     public class Role
     {
+        [Key]
         public int RoleId { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string RoleName { get; set; }
     }
 }
