2c223d89a250645278a6c5f7025d8f40f77590b4
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-11-23 00:15:17 -0700

    added datat annotations to Study model

--- Research_Software_Dev/Research_Software_Dev/Models/Studies/Study.cs
+++ Research_Software_Dev/Research_Software_Dev/Models/Studies/Study.cs
@@ -1,9 +1,15 @@
-﻿namespace Research_Software_Dev.Models.Studies
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Research_Software_Dev.Models.Studies
 {
     public class Study
     {
+        [Key]
         public int StudyId { get; set; }
+        [Required]
+        [StringLength(50)]
         public string StudyName { get; set; }
+        [Required]
         public string StudyDescription { get; set; }
     }
 
