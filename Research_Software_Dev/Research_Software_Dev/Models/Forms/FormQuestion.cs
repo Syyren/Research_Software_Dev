27ad54f0e7b7a1a4724fe1f3f83024d6d52f7673
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-11-23 00:18:15 -0700

    added data annotations to FormQuestion model

--- Research_Software_Dev/Research_Software_Dev/Models/Forms/FormQuestion.cs
+++ Research_Software_Dev/Research_Software_Dev/Models/Forms/FormQuestion.cs
@@ -1,14 +1,23 @@
-﻿namespace Research_Software_Dev.Models.Forms
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Research_Software_Dev.Models.Forms
 {
     public class FormQuestion
     {
-        
+        [Key]
         public int QuestionId { get; set; }
+        [Required]
+        [StringLength(10)]
         public int QuestionNumber { get; set; }
+        [Required]
         public string QuestionDescription { get; set; }
 
         //Form FK
+        [Required]
+        [ForeignKey("Form")]
         public int FormId { get; set; }
+        [Required]
         public Form Form { get; set; }
     }
 }
