26ea75c82d2bcd2cdc07b4a7022a584d0e40e18c
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-11-24 01:06:20 -0700

    Migrations created using the following commands Add-Migration ModelsDefined Update-Database

---
+++ Research_Software_Dev/Research_Software_Dev/Data/Migrations/20241124075017_ModelsDefined.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Research_Software_Dev.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelsDefined : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Forms",
+                columns: table => new
+                {
+                    FormId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FormName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Forms", x => x.FormId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Participants",
+                columns: table => new
+                {
+                    ParticipantId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParticipantFirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ParticipantLastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ParticipantAddress = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    ParticipantEmail = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ParticipantPhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participants", x => x.ParticipantId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Researchers",
+                columns: table => new
+                {
+                    ResearcherId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ResearcherFirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ResearcherLastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ResearcherAddress = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    ResearcherEmail = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ResearcherPhoneNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ResearcherPassword = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Researchers", x => x.ResearcherId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.RoleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Studies",
+                columns: table => new
+                {
+                    StudyId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudyDescription = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Studies", x => x.StudyId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormQuestions",
+                columns: table => new
+                {
+                    QuestionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestionNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    QuestionDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FormId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormQuestions", x => x.QuestionId);
+                    table.ForeignKey(
+                        name: "FK_FormQuestions_Forms_FormId",
+                        column: x => x.FormId,
+                        principalTable: "Forms",
+                        principalColumn: "FormId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResearcherRoles",
+                columns: table => new
+                {
+                    ResearcherId = table.Column<int>(type: "int", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResearcherRoles", x => new { x.ResearcherId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_ResearcherRoles_Researchers_ResearcherId",
+                        column: x => x.ResearcherId,
+                        principalTable: "Researchers",
+                        principalColumn: "ResearcherId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ResearcherRoles_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "RoleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ParticipantStudies",
+                columns: table => new
+                {
+                    ParticipantId = table.Column<int>(type: "int", nullable: false),
+                    StudyId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParticipantStudies", x => new { x.ParticipantId, x.StudyId });
+                    table.ForeignKey(
+                        name: "FK_ParticipantStudies_Participants_ParticipantId",
+                        column: x => x.ParticipantId,
+                        principalTable: "Participants",
+                        principalColumn: "ParticipantId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ParticipantStudies_Studies_StudyId",
+                        column: x => x.StudyId,
+                        principalTable: "Studies",
+                        principalColumn: "StudyId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResearcherStudies",
+                columns: table => new
+                {
+                    ResearcherId = table.Column<int>(type: "int", nullable: false),
+                    StudyId = table.Column<int>(type: "int", nullable: false),
+                    StudyName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResearcherStudies", x => new { x.ResearcherId, x.StudyId });
+                    table.ForeignKey(
+                        name: "FK_ResearcherStudies_Researchers_ResearcherId",
+                        column: x => x.ResearcherId,
+                        principalTable: "Researchers",
+                        principalColumn: "ResearcherId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ResearcherStudies_Studies_StudyId",
+                        column: x => x.StudyId,
+                        principalTable: "Studies",
+                        principalColumn: "StudyId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    SessionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    TimeStart = table.Column<TimeOnly>(type: "time", nullable: false),
+                    TimeEnd = table.Column<TimeOnly>(type: "time", nullable: false),
+                    StudyId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.SessionId);
+                    table.ForeignKey(
+                        name: "FK_Sessions_Studies_StudyId",
+                        column: x => x.StudyId,
+                        principalTable: "Studies",
+                        principalColumn: "StudyId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ParticipantSessions",
+                columns: table => new
+                {
+                    ParticipantId = table.Column<int>(type: "int", nullable: false),
+                    SessionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParticipantSessions", x => new { x.ParticipantId, x.SessionId });
+                    table.ForeignKey(
+                        name: "FK_ParticipantSessions_Participants_ParticipantId",
+                        column: x => x.ParticipantId,
+                        principalTable: "Participants",
+                        principalColumn: "ParticipantId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ParticipantSessions_Sessions_SessionId",
+                        column: x => x.SessionId,
+                        principalTable: "Sessions",
+                        principalColumn: "SessionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResearcherSessions",
+                columns: table => new
+                {
+                    ResearcherId = table.Column<int>(type: "int", nullable: false),
+                    SessionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResearcherSessions", x => new { x.ResearcherId, x.SessionId });
+                    table.ForeignKey(
+                        name: "FK_ResearcherSessions_Researchers_ResearcherId",
+                        column: x => x.ResearcherId,
+                        principalTable: "Researchers",
+                        principalColumn: "ResearcherId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ResearcherSessions_Sessions_SessionId",
+                        column: x => x.SessionId,
+                        principalTable: "Sessions",
+                        principalColumn: "SessionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormAnswers",
+                columns: table => new
+                {
+                    AnswerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Answer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TimeStamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ParticipantId = table.Column<int>(type: "int", nullable: false),
+                    SessionId = table.Column<int>(type: "int", nullable: false),
+                    QuestionId = table.Column<int>(type: "int", nullable: false),
+                    FormQuestionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormAnswers", x => x.AnswerId);
+                    table.ForeignKey(
+                        name: "FK_FormAnswers_FormQuestions_FormQuestionId",
+                        column: x => x.FormQuestionId,
+                        principalTable: "FormQuestions",
+                        principalColumn: "QuestionId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FormAnswers_ParticipantSessions_ParticipantId_SessionId",
+                        columns: x => new { x.ParticipantId, x.SessionId },
+                        principalTable: "ParticipantSessions",
+                        principalColumns: new[] { "ParticipantId", "SessionId" },
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleId", "RoleName" },
+                values: new object[,]
+                {
+                    { 1, "Admin" },
+                    { 2, "Audit" },
+                    { 3, "Level 3" },
+                    { 4, "Level 2" },
+                    { 5, "Level 1" },
+                    { 6, "Level 0" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormAnswers_FormQuestionId",
+                table: "FormAnswers",
+                column: "FormQuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormAnswers_ParticipantId_SessionId",
+                table: "FormAnswers",
+                columns: new[] { "ParticipantId", "SessionId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormQuestions_FormId",
+                table: "FormQuestions",
+                column: "FormId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParticipantSessions_SessionId",
+                table: "ParticipantSessions",
+                column: "SessionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParticipantStudies_StudyId",
+                table: "ParticipantStudies",
+                column: "StudyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResearcherRoles_RoleId",
+                table: "ResearcherRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResearcherSessions_SessionId",
+                table: "ResearcherSessions",
+                column: "SessionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResearcherStudies_StudyId",
+                table: "ResearcherStudies",
+                column: "StudyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_StudyId",
+                table: "Sessions",
+                column: "StudyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FormAnswers");
+
+            migrationBuilder.DropTable(
+                name: "ParticipantStudies");
+
+            migrationBuilder.DropTable(
+                name: "ResearcherRoles");
+
+            migrationBuilder.DropTable(
+                name: "ResearcherSessions");
+
+            migrationBuilder.DropTable(
+                name: "ResearcherStudies");
+
+            migrationBuilder.DropTable(
+                name: "FormQuestions");
+
+            migrationBuilder.DropTable(
+                name: "ParticipantSessions");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Researchers");
+
+            migrationBuilder.DropTable(
+                name: "Forms");
+
+            migrationBuilder.DropTable(
+                name: "Participants");
+
+            migrationBuilder.DropTable(
+                name: "Sessions");
+
+            migrationBuilder.DropTable(
+                name: "Studies");
+        }
+    }
+}
