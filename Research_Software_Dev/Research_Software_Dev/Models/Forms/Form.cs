0f8054c37cd2361948d1661c74d1fe94a9e743e0
Ryan M <129496054+Syyren@users.noreply.github.com>
2024-11-27 16:09:31 -0700

    updated theme and general site aesthetics

--- Research_Software_Dev/Research_Software_Dev/Models/Forms/Form.cs
+++ Research_Software_Dev/Research_Software_Dev/Models/Forms/Form.cs
@@ -8,10 +8,10 @@ namespace Research_Software_Dev.Models.Forms
         public string FormId { get; set; }
 
         [Required]
-        [Display(Name = "Form Name")]
+        [Display(Name = "Name")]
         public string FormName { get; set; }
 
-        [Display(Name = "Form Questions")]
+        [Display(Name = "Questions")]
         public ICollection<FormQuestion> Questions { get; set; } = new List<FormQuestion>();
 
 
